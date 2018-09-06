21745c2cb6da21e05e926e5706a9698ef73c531d
Belen Buffa <belenbuffa@gmail.com>
2018-09-06 18:15:09 -0300

    webgl

---
+++ webgl/js/webgl.js
@@ -0,0 +1,63 @@
+var triangleVertexPositionBuffer;
+var squareVertexPositionBuffer;
+
+$(document).ready(function() {
+
+    webGLStart();
+
+});
+
+function webGLStart() {
+    var canvas = $('#canvas');
+    
+    initGL(canvas);
+    initShaders();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    drawScene();
+}
+
+function initBuffers() {
+    triangleVertexPositionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+
+    var vertices = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    triangleVertexPositionBuffer.numItems = 3;
+    triangleVertexPositionBuffer.itemSize = 3;
+
+    squareVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    vertices = [
+        1.0,  1.0,  0.0,
+        -1.0,  1.0,  0.0,
+         1.0, -1.0,  0.0,
+        -1.0, -1.0,  0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    squareVertexPositionBuffer.numItems = 4;
+    squareVertexPositionBuffer.itemSize = 3;
+}
+
+function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeigth);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+    mat4.identity(mvMatrix);
+
+    
+
+}
+
+
+
